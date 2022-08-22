965e068d32446ee9df2806442a42a4cfa9a129fb
Erik Lisboa <bonitoerik@gmail.com>
2022-08-22 11:27:21 -0300

    Adicionando meu primeiro arquivo c#

---
+++ comparador_idades.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicional1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual sua idade");
+            int ida = int.Parse(Console.ReadLine());
+            if(ida == 0)
+            {
+                Console.WriteLine("Retorne uma idade correta");
+            }
+            else if (ida >= 1 && ida <= 12)
+            {
+                Console.WriteLine("Voce é uma criança de " + ida +" anos");
+            }
+            else if(ida > 12 && ida <= 18)
+            {
+                Console.WriteLine("Voce é um adolecente de " + ida +" anos");
+            }
+            else if(ida >18 && ida < 60)
+            {
+                Console.WriteLine("Voce é um adulto de " + ida + " anos");
+            }
+            else 
+            {
+                Console.WriteLine("Voce é um idoso de " + ida + " anos");
+            }
+            Console.ReadLine();
+        }
+    }
+}
